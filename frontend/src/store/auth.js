c6477897d4a765f858ac279ec66160fe0e9c664f
mihir3030 <mihirdholakia777@gmail.com>
2025-07-15 09:54:39 +0530

    Zustand create auth store

---
+++ frontend/src/store/auth.js
@@ -0,0 +1,27 @@
+import { create } from 'zustand'
+import { mountStoreDevtool } from 'simple-zustand-devtools'
+
+
+// set for update state. get for getting current data
+const useAuthStore = create((set, get) => ({
+    allUserdata: null,
+    loading: false,
+
+    // get user info
+    user: () => ({
+        user_id: get().allUserdata?.user_id || null,
+        username: get().allUserdata?.username || null,
+    }),
+
+    // when call replace old with new user data
+    setUser: (user) => set({ allUserdata: user }),
+    setLoading: (loading) => set({ loading }),
+    setLoggedIn: () => get().allUserdata !== null
+
+}))
+
+if (import.meta.env.DEV) {
+    mountStoreDevtool('Store', useAuthStore)
+}
+
+export { useAuthStore }
