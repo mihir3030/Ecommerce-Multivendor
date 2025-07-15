8a75d78cf60a733e6ef8f64ff75a2922945aa837
mihir3030 <mihirdholakia777@gmail.com>
2025-07-15 09:29:50 +0530

    Register serializer added

--- backend/userauths/serializers.py
+++ backend/userauths/serializers.py
@@ -1,9 +1,55 @@
+from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 from userauths.models import User, Profile
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        token['full_name'] = user.full_name
+        token['email'] = user.email
+        token['username'] = user.username
+        try:
+            token['vendor_id'] = user.vendor.id
+        except:
+            token['vendor_id'] = 0
+        
+        return token
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ["full_name", "email", "phone", "password", "password2"]
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password: Password Does not match"})
+        
+        return attrs
+    
+    def create(self, validated_data):
+        user = User.objects.create(
+            full_name=validated_data['full_name'],
+            email=validated_data['email'],
+            phone=validated_data['phone']
+        )
+
+        email_user, domain_name = user.email.split("@")
+        user.username = email_user
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
 
 
 # serializers converts pyhona object or any complex data into Json
-
 class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
--- backend/userauths/views.py
+++ backend/userauths/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, AllowAny
+
+from userauths.models import User, Profile
+from userauths.serializers import MyTokenObtainPairSerializer, RegisterSerializer
+
 
 # Create your views here.
+#  this is for token generate view
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
+
+########################################################
+#---------------------REGISTER -------------------------#
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny, )
+    serializer_class = RegisterSerializer
